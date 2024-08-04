1fc4a81afa2aa70b9946e37d92de7e1c3c864ca1
Wix <shendrickwilliams@gmail.com>
2024-08-04 20:20:47 +0200

    files

---
+++ js/api.js
@@ -0,0 +1,8 @@
+// Primary API https://opendata.adsb.fi/api/v2/lat/52.5105/lon/6.0946/dist/25
+// Backup API https://api.adsb.lol/v2/lat/52.5105/lon/6.0946/dist/25
+
+const home = {
+    latitude: 52.5105,
+    longitude: 6.0946
+}
+
