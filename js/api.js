0bfc670197fd01ab802a663964bb8c19cdf79e99
Wix <shendrickwilliams@gmail.com>
2024-08-04 21:37:36 +0200

    API functions, attribution edit and styling

--- js/api.js
+++ js/api.js
@@ -1,8 +1,18 @@
-// Primary API https://opendata.adsb.fi/api/v2/lat/52.5105/lon/6.0946/dist/25
-// Backup API https://api.adsb.lol/v2/lat/52.5105/lon/6.0946/dist/25
+// Primary API https://api.adsb.lol/v2/lat/52.5105/lon/6.0946/dist/25
+// Backup API https://opendata.adsb.fi/api/v2/lat/52.5105/lon/6.0946/dist/25
 
 const home = {
     latitude: 52.5105,
     longitude: 6.0946
+}
+
+function singleClosestAircraft () {
+    // https://api.adsb.lol/v2/closest/52.5105/6.0946/25
+
+}
+
+function multipleClosestAircraft() {
+    // https://api.adsb.lol/v2/lat/52.5105/lon/6.0946/dist/25
+
 }
 
