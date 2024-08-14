63c4ed7e34d62839f462358eeb16d571a80b9998
Wix <shendrickwilliams@gmail.com>
2024-08-14 11:31:14 +0200

    single aircraft API call

--- js/api.js
+++ js/api.js
@@ -1,18 +1,31 @@
 // Primary API https://api.adsb.lol/v2/lat/52.5105/lon/6.0946/dist/25
 // Backup API https://opendata.adsb.fi/api/v2/lat/52.5105/lon/6.0946/dist/25
 
-const home = {
+const loc = {
     latitude: 52.5105,
-    longitude: 6.0946
+    longitude: 6.0946,
+    radius: 25
 }
 
-function singleClosestAircraft () {
-    // https://api.adsb.lol/v2/closest/52.5105/6.0946/25
+fetchSingleClosestAircraft()
 
+async function fetchSingleClosestAircraft() {
+    const url = 'https://corsproxy.io/?' + encodeURIComponent('https://opendata.adsb.fi/api/v2/lat/52.5105/lon/6.0946/dist/25');
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error("Could not fetch data");
+    }
+
+    const data = await response.json();
+
+    const flight = data.aircraft[0]?.flight;
+
+    console.log(flight);
 }
 
 function multipleClosestAircraft() {
     // https://api.adsb.lol/v2/lat/52.5105/lon/6.0946/dist/25
 
 }
-
