5d5c1d5258b6ea18b3081b2565898fcc66f93258
Wix <shendrickwilliams@gmail.com>
2024-08-25 18:42:37 +0200

    clean up

--- js/index.js
+++ js/index.js
@@ -3,9 +3,6 @@ import * as api from './api.js';
 const singleButton = document.getElementById('singleaircraft');
 const multiButton = document.getElementById('multiaircraft');
 
-// Initialize the map
-
-// Event listener for single aircraft
 singleButton.addEventListener('click', async() => {
     try {
         const singleResponse = await api.fetchSingleClosestAircraft();
@@ -23,7 +20,6 @@ singleButton.addEventListener('click', async() => {
     }
 });
 
-// Event listener for multiple aircraft
 multiButton.addEventListener('click', async() => {
     try {
         const multiResponse = await api.multipleClosestAircraft();
@@ -31,9 +27,8 @@ multiButton.addEventListener('click', async() => {
         if (multiResponse) {
             for (let x = 0; x < multiResponse.ac.length; x++) {
                 const aircraft = multiResponse.ac[x];
-                const { lat, lon, flight, alt_baro, true_heading } = aircraft; // Assuming each aircraft has lat, lon, and flight properties
+                const { lat, lon, flight, alt_baro, true_heading } = aircraft;
 
-                // Create a new marker for each aircraft on the map
                 var aircraftMarker = L.marker([lat, lon]).addTo(map);
                 aircraftMarker.bindPopup(`Flight: ${flight}<br>Altitude: ${alt_baro} ft<br>Heading: ${true_heading}°`).openPopup();
             }
